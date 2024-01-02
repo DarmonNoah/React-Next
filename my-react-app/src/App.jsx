e49f65c8e2ddd714d6099c475d75b00a8a48aebe
DarmonNoah <noah.darmon@ecole-isitech.fr>
2024-01-02 11:27:14 +0100

    Update profile&App

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css';
+import Profile from './components/profile';
 
 function App() {
-const [name, setName]=useState("React")
-console.log(name);
+  const [name, setName]=useState("React")
+ // console.log(name);
 
 function onClickHandler(){
-  name="isitech"
-  console.log(name);
+  setName("Vite");
+
+  setName((prev)=>{return prev + "vite";})
 }
 
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">{name}</div>
-        <button onClick={onClickHandler()}>click me ! </button>
+        <button onClick={onClickHandler}>click me ! </button>
+        <Profile/>
     </>
   )
 }
